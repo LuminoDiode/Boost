b4ecfbbf4f70c48ea2017bd182db385a19fb57ed
Rodion Ryzhov <65biso2@gmail.com>
2021-09-05 15:05:04 +0300

    class ScrollableStackPanel added

---
+++ Controls/Controls.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms.VisualStyles;
+
+namespace Boost
+{
+	public partial class Controls
+	{
+		public static Panel ToVerticalStackPanel(IList<Control> Controls)
+		{
+			Panel Out = new Panel();
+			if (Controls.Count > 0)
+			{
+				int CurrentHeight = 0;
+
+				for (int i = 0; i < Controls.Count; i++)
+				{
+					Controls[i].Location = new Point(0, CurrentHeight);
+					Out.Controls.Add(Controls[i]);
+					CurrentHeight += Controls[i].Height;
+				}
+
+				Out.Height = Controls.Last().Location.Y + Controls.Last().Height;
+				Out.Width = Controls.Max(x => x.Width);
+				Out.BackColor = Color.Transparent;
+			}
+			return Out;
+		}
+		public static Panel ToHorizontalStackPanel(IList<Control> Controls, int space = 0)
+		{
+			Panel Out = new Panel();
+			int CurrentWidth = 0;
+			
+			for (int i = 0; i < Controls.Count; i++)
+			{
+				Controls[i].Location = new Point(CurrentWidth + (i==0? 0:space), 0);
+				Out.Controls.Add(Controls[i]);
+				CurrentWidth += Controls[i].Width;
+			}
+
+			Out.Width = CurrentWidth;
+			Out.Height = Controls.Max(x => x.Height);
+			Out.BackColor = Color.Transparent;
+
+			return Out;
+		}
+
+		public static void ToSameWidth(IList<Control> Controls)
+		{
+			int MaxWidth = 0;
+			for (int i = 0; i < Controls.Count; i++) if (Controls[i].Width > MaxWidth) MaxWidth = Controls[i].Width;
+			for (int i = 0; i < Controls.Count; i++) Controls[i].Width = MaxWidth;
+		}
+		public static void ToSameWidth(IList<Control> Controls, int Width)
+		{
+			for (int i = 0; i < Controls.Count; i++) Controls[i].Width = Width;
+		}
+		public static void ToSameWidth(params Control[] Controls)
+		{
+			ToSameWidth(Controls as IList<Control>);
+		}
+
+		class VarInputField
+		{
+			private static readonly Label EqualsSymb = new Label { Text = "=" };
+			private Label label = new Label();
+			private TextBox textbox = new TextBox();
+
+			public string Name
+			{
+				get => label.Text;
+				set => label.Text = value;
+			}
+
+			public string Value
+			{
+				get => textbox.Text;
+				set => textbox.Text = value;
+			}
+
+			public VarInputField(string Name, int TextBoxWidth)
+			{
+				label.Text = Name;
+				textbox.Width = TextBoxWidth;
+			}
+
+			public Panel GetControl()
+			{
+				return ToHorizontalStackPanel(new Control[] { label, EqualsSymb, textbox });
+			}
+		}
+
+		public static ScrollableControl ToScrollableControl(IList<Control> Controls, Size ScrollableControlSize, int space = 5, int AddSpacesAtEnd = 1)
+		{
+			ScrollableControl Out = new ScrollableControl();
+			Out.Size = ScrollableControlSize;
+			Out.AutoScroll = true;
+
+			int CurHei = space, CurWid = space;
+			for (int i = 0; i < Controls.Count; i++)
+			{
+				var temp = Controls[i];
+				temp.Location = new Point(CurWid, CurHei); CurHei += 5 + temp.Height;
+				Out.Controls.Add(temp);
+			}
+			Out.Controls.Add(new Panel { Size = new Size(0, AddSpacesAtEnd * space), Location = new Point(CurWid, CurHei) });
+			return Out;
+		}
+
+	}
+	
+}
---
+++ Controls/ScrollableStackPanel.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms.VisualStyles;
+
+namespace Boost
+{
+	public partial class Controls
+	{
+		public class ScrollableStackPanel
+		{
+			//public readonly ICollection<Control> Controls => this.MainControl.Co
+			private readonly ScrollableControl MainControl;
+			public ScrollableControl AsControl => this.MainControl;
+
+			private bool _AutoWidth;
+			public bool AutoWidth
+			{
+				get => this._AutoWidth;
+				set
+				{
+					if (value == true)
+					{
+						this.SetAutoWidth();
+						this._AutoWidth = value;
+					}
+					else
+					{
+						this.MainControl.Width = this._UserWidth;
+					}
+				}
+			}
+
+			private int _UserWidth;
+			public int Width
+			{
+				get => this.MainControl.Width;
+				set
+				{
+					this._UserWidth = value;
+					if (!AutoWidth) this.MainControl.Width = value;
+				}
+			}
+			public int Height
+			{
+				get => this.MainControl.Height;
+				set => this.MainControl.Height = value;
+			}
+
+			private Point LowestElementLocation => this.MainControl.Controls.Count == 0 ? new Point(0, 0) : this.MainControl.Controls.Last().Location;
+			private Size LowestElementSize => this.MainControl.Controls.Count == 0 ? new Size(0, 0) : this.MainControl.Controls.Last().Size;
+			public Point LowestLeftPointOfControls => new Point(0, LowestElementLocation.Y + LowestElementSize.Height);
+
+			public static explicit operator ScrollableControl(ScrollableStackPanel SSP) => SSP.MainControl;
+			public static explicit operator Control(ScrollableStackPanel SSP) => SSP.MainControl;
+
+			public ScrollableStackPanel(int height)
+			{
+				this.MainControl = new ScrollableControl { Height = height, AutoScroll = true };
+			}
+			public ScrollableStackPanel(Control[] controls, int height)
+			{
+				foreach (Control c in controls) this.MainControl.Controls.Add(c);
+				this.MainControl = new ScrollableControl { Height = height, AutoScroll = true };
+			}
+
+			public void AddControl(Control Cntrl)
+			{
+				Cntrl.Location = this.MainControl.Controls.Count > 0 ? this.LowestLeftPointOfControls : new Point(0, 0);
+
+				Controls.Add(Cntrl);
+				this.MainControl.Controls.Add(Cntrl);
+				SetAutoWidthIfNeeded();
+			}
+
+			public void AddControl(Control Cntrl, int Index)
+			{
+				if (Index > (this.MainControl.Controls.Count - 1))
+				{
+					this.AddControl(Cntrl);
+					return;
+				}
+				Cntrl.Location = this.MainControl.Controls[Index].Location;
+				Controls.Insert(Index, Cntrl);
+				this.MainControl.Controls.Add(Cntrl);
+				for (int i = Index + 1; i < this.MainControl.Controls.Count; i++)
+				{
+					this.MainControl.Controls[i].Location = Point.Add(this.MainControl.Controls[i].Location, new Size(0, Cntrl.Size.Height));
+				}
+			}
+			/// <summary>
+			/// Removes the first occurrence
+			/// </summary>
+			/// <param name="Cntrl"></param>
+			public void RemoveControl(Control Cntrl)
+			{
+				var RemovedSize = Cntrl.Size;
+				var RemovedIndex = this.MainControl.Controls.IndexOf(Cntrl);
+				this.MainControl.Controls.Remove(Cntrl);
+				for (int i = RemovedIndex; i < this.MainControl.Controls.Count; i++)
+				{
+					this.MainControl.Controls[i].Location = new Point(0,RemovedSize.Height);
+				}
+			}
+
+			public void RemoveControl(int Index)
+			{
+
+			}
+
+			public Control GetControl(int Index)
+			{
+				return this.MainControl.Controls[Index];
+			}
+			private void SetAutoWidthIfNeeded()
+			{
+				if (this.AutoWidth) this.SetAutoWidth();
+			}
+			private void SetAutoWidth()
+			{
+				if (this.MainControl.Controls.Count > 0) this.MainControl.Width = ((ICollection<Control>)this.MainControl.Controls).Max(x => x.Width) + 17;
+			}
+		}
+	}
+}
